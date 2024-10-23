0f286836e469a674e605b1bc9d938db0e880beea
Zayn365 <zaynsaeed365@gmail.com>
2024-10-24 02:13:12 +0500

    Update: Live axios link added

--- utils/Axios.ts
+++ utils/Axios.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 const local = "http://localhost:4000";
 const live = "https://ai-project-backend-rho.vercel.app";
 export const Axios = axios.create({
-  baseURL: local,
+  baseURL: live,
 });
