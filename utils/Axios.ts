ba724ac6e3e3cdbc3cb025b9b43b7634e2bf1567
Abdul Fatir Ansari <abdulfatiransari2@gmail.com>
2025-11-11 00:23:52 +0500

    fix: add ssl in url

--- utils/Axios.ts
+++ utils/Axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const local = "http://localhost:5000";
 const live = "https://ai-project-backend-rho.vercel.app";
 const ec2 = "http://18.208.248.57:5000";
-const ngrok = "https://5dd69660dce5.ngrok-free.app";
+const nginx = "https://18.208.248.57";
 export const Axios = axios.create({
-  baseURL: ngrok,
+  baseURL: nginx,
 });
