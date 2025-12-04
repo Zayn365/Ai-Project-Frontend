21a005a280361d59e225a343f2092626121cb77e
Abdul Fatir Ansari <abdulfatiransari2@gmail.com>
2025-12-04 20:38:32 +0500

    fix: change url

--- utils/Axios.ts
+++ utils/Axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 const local = "http://localhost:5000";
 const live = "https://ai-project-backend-rho.vercel.app";
-const ec2 = "http://18.208.248.57:5000";
+const ec2 = "http://localhost:5000";
 const nginx = "https://18.208.248.57";
 export const Axios = axios.create({
-  baseURL: nginx,
+  baseURL: ec2,
 });
