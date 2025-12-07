28bb25ba328b01a95ab9bd6847fc2008223e5d55
Abdul Fatir Ansari <abdulfatiransari2@gmail.com>
2025-12-07 23:07:09 +0500

    fix: change the live url

--- utils/Axios.ts
+++ utils/Axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const local = "http://localhost:5000";
 const live = "https://ai-project-backend-rho.vercel.app";
-const ec2 = "http://localhost:5000";
+const ec2 = "https://api.narrativemaster.com";
 const nginx = "https://18.208.248.57";
 export const Axios = axios.create({
   baseURL: ec2,
